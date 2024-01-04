2417658a186b57b81cc8a69a0c7ca5f1df04ddb4
tongyao-zhu <312404930zhu@gmail.com>
2024-01-04 11:45:36 +0800

    add pretraining scripts

---
+++ scripts/pretraining.sh
@@ -0,0 +1,23 @@
+export WANDB_PROJECT=RetPretrain
+export WANDB_API_KEY=5723c6f7e50618fd5dd5a9c2dc2c293f293a25e6
+DATASET_NAME=$1
+FULL_DATA_PATH=/home/aiops/zhuty/ret_pretraining_data/$DATASET_NAME\_sample_processed
+export MODEL_NAME="tiny_LLaMA_120M"
+export WANDB_NAME=$MODEL_NAME\_$DATASET_NAME
+export NUMBER_OF_GPU=$(python -c "import torch; print(torch.cuda.device_count())")
+echo "Using $NUMBER_OF_GPU GPUs"
+echo "WANDB_NAME=$WANDB_NAME"
+
+lightning run model \
+    --node-rank=0  \
+    --main-address=127.0.01 \
+    --accelerator=cuda \
+    --num-nodes=1 \
+    --devices=$NUMBER_OF_GPU \
+    pretrain/tinyllama.py --devices $NUMBER_OF_GPU \
+    --train_data_dir $FULL_DATA_PATH \
+    --val_data_dir $FULL_DATA_PATH
+
+# sample usage
+# bash scripts/pretraining.sh redpajama_2b
+# bash scripts/pretraining.sh redpajama_2b_reordered_train_top10
