795f5d511670088296ff6c6d9dc26b07740082be
tongyao-zhu <312404930zhu@gmail.com>
2024-01-05 10:56:52 +0800

    make more general form of pretraining

--- scripts/pretraining.sh
+++ scripts/pretraining.sh
@@ -1,8 +1,16 @@
 export WANDB_PROJECT=RetPretrain
 export WANDB_API_KEY=5723c6f7e50618fd5dd5a9c2dc2c293f293a25e6
-DATASET_NAME=$1
+export DATASET_NAME=$2
 FULL_DATA_PATH=/home/aiops/zhuty/ret_pretraining_data/$DATASET_NAME\_sample_processed
-export MODEL_NAME="tiny_LLaMA_120M"
+export MODEL_NAME=$1
+
+# ensure that the model name is valid , either 'tiny_LLaMA_1b' or 'tiny_LLaMA_120M' or 'tiny_LLaMA_120M_4k'
+if [ $MODEL_NAME != "tiny_LLaMA_1b" ] && [ $MODEL_NAME != "tiny_LLaMA_120M" ] && [ $MODEL_NAME != "tiny_LLaMA_120M_4k" ]; then
+    echo "Error: '$MODEL_NAME' is not a valid model name."
+    exit 1
+fi
+
+
 export WANDB_NAME=$MODEL_NAME\_$DATASET_NAME
 export NUMBER_OF_GPU=$(python -c "import torch; print(torch.cuda.device_count())")
 echo "Using $NUMBER_OF_GPU GPUs"
