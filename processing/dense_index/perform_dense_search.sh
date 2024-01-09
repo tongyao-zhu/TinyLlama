079844cbb98b39f9c7c578f40c30378f8bccacb9
tongyao-zhu <312404930zhu@gmail.com>
2024-01-09 11:40:56 +0800

    add more general version of dense search

--- processing/dense_index/perform_dense_search.sh
+++ processing/dense_index/perform_dense_search.sh
@@ -5,15 +5,17 @@ export PATH=$PATH:$JAVA_HOME/bin
 
 
 CHUNK_NUM=$1
+DATASET_NAME=$2
 
-INDEX_PATH=/home/aiops/zhuty/ret_pretraining_data/redpajama_2b_id_added/dense_index/shard_full
-QUERY_PATH=/home/aiops/zhuty/ret_pretraining_data/redpajama_2b_id_added/queries/chunk_$CHUNK_NUM.jsonl
-OUT_PATH=/home/aiops/zhuty/ret_pretraining_data/redpajama_2b_id_added/dense_search_results/chunk_$CHUNK_NUM.result.txt
+INDEX_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/dense_index/shard_full
+QUERY_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/queries/chunk_$CHUNK_NUM.jsonl
+OUT_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/dense_search_results/chunk_$CHUNK_NUM.result.txt
 
 python -m pyserini.search.faiss \
   --index $INDEX_PATH \
   --topics $QUERY_PATH \
   --output  $OUT_PATH \
   --encoder facebook/contriever \
-  --batch-size 256 --threads 96 --hits 100 \
+  --encoded-queries /home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/dense_queries/chunk_$CHUNK_NUM \
+  --batch-size 128 --threads 96 --hits 100 \
   --device cuda:0
--- processing/dense_index/range_search_dense.sh
+++ processing/dense_index/range_search_dense.sh
@@ -2,7 +2,8 @@ export JAVA_HOME=/home/aiops/zhuty/jdk-11.0.20
 export PATH=$PATH:$JAVA_HOME/bin
 left=$1
 right=$2
+DATASET_NAME=$3
 for chunk_num in $(seq $left $right); do
   echo "chunk num" $chunk_num
-  bash perform_dense_search.sh $chunk_num ;
+  bash perform_dense_search.sh $chunk_num $DATASET_NAME ;
 done
