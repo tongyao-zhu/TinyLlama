b311ceb030eabaa8afbc795cd36539efdae56420
tongyao-zhu <312404930zhu@gmail.com>
2024-01-09 11:46:43 +0800

    add a more general version of creating data

--- processing/build_bm25_index.sh
+++ processing/build_bm25_index.sh
@@ -3,9 +3,10 @@ export PATH=$PATH:$JAVA_HOME/bin
 
 #INPUT_PATH=/home/aiops/zhuty/ret_pretraining_data/redpajama_20b_id_added/train
 #INDEX_PATH=/home/aiops/zhuty/ret_pretraining_data/redpajama_20b_id_added/bm25_index
+VERSION=$1
 
-INPUT_PATH=/home/aiops/zhuty/ret_pretraining_data/redpajama_2b_id_added/train
-INDEX_PATH=/home/aiops/zhuty/ret_pretraining_data/redpajama_2b_id_added/bm25_index
+INPUT_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$VERSION/train
+INDEX_PATH=/home/aiops/zhuty/ret_pretraining_data//id_added/$VERSION/bm25_index
 python -m pyserini.index.lucene \
   --collection JsonCollection \
   --input $INPUT_PATH \
