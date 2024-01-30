5a70aa9ec5b2f8c4c5ec9e9dfd87fc70d935e723
tongyao-zhu <312404930zhu@gmail.com>
2024-01-30 15:01:54 +0800

    Update convert_to_hf.sh

--- scripts/convert_to_hf.sh
+++ scripts/convert_to_hf.sh
@@ -4,13 +4,20 @@
 #--model_name tiny_LLaMA_120M \
 #--model_only false
 
-LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tinyllaMA_1b
-CKPT_PREFIX=iter-020000-ckpt
+# LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tinyllaMA_1b
+#LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tiny_LLaMA_120M_8k_rpwiki_en_8k
+#CKPT_PREFIX=iter-020000-ckpt
+
+LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tiny_LLaMA_360M_8k_c4_news_8k
+CKPT_PREFIX=iter-030000-ckpt
+
+LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tiny_LLaMA_120M_8k_c4_news_8k
+CKPT_PREFIX=iter-030000-ckpt
 
 python convert_lit_checkpoint.py \
 --checkpoint_name $CKPT_PREFIX.pth \
 --out_dir $LIT_CKPT_DIR \
---model_name tiny_LLaMA_1b \
+--model_name tiny_LLaMA_120M_8k \
 --model_only false
 
 mkdir $LIT_CKPT_DIR/hf_ckpt
