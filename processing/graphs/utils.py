15dae81b9a869159401fcde76278ee3435cc9155
tongyao-zhu <312404930zhu@gmail.com>
2024-01-30 14:59:31 +0800

    add function to get stats from adj list file

--- processing/graphs/utils.py
+++ processing/graphs/utils.py
@@ -1,6 +1,57 @@
 import json
 import os
 import tqdm
+from collections import defaultdict
+import numpy as np
+
+def get_path_stats(result_paths):
+    # print the distribution of lengths in result_paths
+    print("Number of paths:", len(result_paths))
+    print("Average length:", sum([len(path) for path in result_paths]) / len(result_paths), "standard deviation:", np.std([len(path) for path in result_paths]))
+    print("Maximum length:", max([len(path) for path in result_paths]))
+    print("Number of paths with length 1:", len([path for path in result_paths if len(path) == 1]))
+    print("% of paths with length 1:", len([path for path in result_paths if len(path) == 1]) / len(result_paths)*100, "%")
+    print("Top 10 paths length:", sorted([len(path) for path in result_paths], reverse=True)[:10])
+    print("Bottom 10 paths length:", sorted([len(path) for path in result_paths])[:10])
+    stats = {
+        'num_paths': len(result_paths),
+        'avg_length': sum([len(path) for path in result_paths]) / len(result_paths),
+        'std_length': np.std([len(path) for path in result_paths]),
+        'max_length': max([len(path) for path in result_paths]),
+        'num_paths_length_1': len([path for path in result_paths if len(path) == 1]),
+        'percent_paths_length_1': len([path for path in result_paths if len(path) == 1]) / len(result_paths)
+    }
+    return stats
+
+def read_jsonl_files(jsonl_dir):
+    adj_list = {}
+    for file_name in tqdm.tqdm(os.listdir(jsonl_dir)):
+        file_path = os.path.join(jsonl_dir, file_name)
+        with open(file_path) as f:
+            for line in f:
+                line = json.loads(line)
+                adj_list[line['query_id']] = line['docs']
+    print("Read", len(adj_list), "queries from", jsonl_dir)
+    print("Read {} files from {}".format(len(os.listdir(jsonl_dir)), jsonl_dir))
+    return adj_list
+
+def read_adj_lst(adj_lst_file):
+    """
+    Supports reading both a single json file and a directory of jsonl files
+    Args:
+        adj_lst_file:
+
+    Returns: adj_lst
+
+    """
+    if adj_lst_file.endswith('.json'):
+        # read the adjacency list
+        adj_lst = json.load(open(adj_lst_file))
+    elif os.path.isdir(adj_lst_file):
+        adj_lst = read_jsonl_files(adj_lst_file)
+    else:
+        raise ValueError("Invalid adj list file")
+    return adj_lst
 
 def get_file_ids(directory_path):
     file_ids = set()
