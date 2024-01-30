dafe779658e7ba68e46a4e9b3117b537e45408f8
tongyao-zhu <312404930zhu@gmail.com>
2024-01-30 15:11:49 +0800

    make encode queries be able to handle range

--- processing/dense_index/encode_queries.sh
+++ processing/dense_index/encode_queries.sh
@@ -3,15 +3,24 @@
 export JAVA_HOME=/home/aiops/zhuty/jdk-11.0.20
 export PATH=$PATH:$JAVA_HOME/bin
 
+DATASET_NAME=$1
+QUERY_TYPE=$2
+CHUNK_NUM_LOWER=$3
+CHUNK_NUM_UPPER=$4
 
-CHUNK_NUM=$1
-DATASET_NAME=$2
+mkdir /home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/dense_queries/$QUERY_TYPE
 
-QUERY_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/queries/chunk_$CHUNK_NUM.jsonl
-OUT_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/dense_queries/chunk_$CHUNK_NUM
-
-python encode_queries.py \
-  --topics $QUERY_PATH \
-  --output  $OUT_PATH \
-  --encoder facebook/contriever \
-  --device cuda:0
+for CHUNK_NUM in $(seq $CHUNK_NUM_LOWER $CHUNK_NUM_UPPER); do
+  QUERY_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/queries/$QUERY_TYPE/chunk_$CHUNK_NUM.jsonl
+  OUT_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/dense_queries/$QUERY_TYPE/chunk_$CHUNK_NUM
+  # if output path is a directory, skip
+  if [ -d "$OUT_PATH" ]; then
+    echo "$OUT_PATH exists, skip"
+    continue
+  fi
+  python encode_queries.py \
+    --topics $QUERY_PATH \
+    --output  $OUT_PATH \
+    --encoder facebook/contriever \
+    --device cuda:0
+done
