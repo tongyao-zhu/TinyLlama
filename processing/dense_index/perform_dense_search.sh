c05554cb2b3b0902d64870d2ee482469bfbe7e33
tongyao-zhu <312404930zhu@gmail.com>
2024-01-30 15:11:20 +0800

    update dense search scripts

--- processing/dense_index/perform_dense_search.sh
+++ processing/dense_index/perform_dense_search.sh
@@ -3,19 +3,28 @@
 export JAVA_HOME=/home/aiops/zhuty/jdk-11.0.20
 export PATH=$PATH:$JAVA_HOME/bin
 
-
-CHUNK_NUM=$1
-DATASET_NAME=$2
+DATASET_NAME=$1
+QUERY_TYPE=$2
+CHUNK_NUM=$3
 
 INDEX_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/dense_index/shard_full
-QUERY_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/queries/chunk_$CHUNK_NUM.jsonl
-OUT_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/dense_search_results/chunk_$CHUNK_NUM.result.txt
+QUERY_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/queries/$QUERY_TYPE/chunk_$CHUNK_NUM.jsonl
+OUT_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/dense_search_results/$QUERY_TYPE/chunk_$CHUNK_NUM.result.txt
+# if it exists, skip
+if [ -f "$OUT_PATH" ]; then
+  echo "$OUT_PATH exists, skip"
+  exit 0
+fi
 
 python -m pyserini.search.faiss \
   --index $INDEX_PATH \
   --topics $QUERY_PATH \
   --output  $OUT_PATH \
   --encoder facebook/contriever \
-  --encoded-queries /home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/dense_queries/chunk_$CHUNK_NUM \
-  --batch-size 128 --threads 96 --hits 100 \
+  --encoded-queries /home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/dense_queries/$QUERY_TYPE/chunk_$CHUNK_NUM \
+  --batch-size 1024 --threads 96 --hits 100 \
   --device cuda:0
+
+
+#   --encoded-queries /home/aiops/zhuty/ret_pretraining_data/id_added/$DATASET_NAME/dense_queries/chunk_$CHUNK_NUM \
+# --nprobes 64
--- processing/dense_index/range_search_dense.sh
+++ processing/dense_index/range_search_dense.sh
@@ -1,9 +1,11 @@
 export JAVA_HOME=/home/aiops/zhuty/jdk-11.0.20
 export PATH=$PATH:$JAVA_HOME/bin
-left=$1
-right=$2
-DATASET_NAME=$3
+DATASET_NAME=$1
+QUERY_TYPE=$2
+left=$3
+right=$4
+
 for chunk_num in $(seq $left $right); do
   echo "chunk num" $chunk_num
-  bash perform_dense_search.sh $chunk_num $DATASET_NAME ;
+  bash perform_dense_search.sh  $DATASET_NAME $QUERY_TYPE $chunk_num ;
 done
