5c5677b7d716d974771068437e04d510e4b0b720
tongyao-zhu <312404930zhu@gmail.com>
2024-01-30 14:58:48 +0800

    add query version as a parameter

--- processing/range_search_bm25.sh
+++ processing/range_search_bm25.sh
@@ -1,9 +1,10 @@
 export JAVA_HOME=/home/aiops/zhuty/jdk-11.0.20
 export PATH=$PATH:$JAVA_HOME/bin
-VERSION=$1
-left=$2
-right=$3
+DATA_VERSION=$1
+QUERY_VERSION=$2
+left=$3
+right=$4
 for chunk_num in $(seq $left $right); do
   echo "chunk num" $chunk_num
-  bash search_bm25_general.sh $chunk_num $VERSION ;
+  bash search_bm25_general.sh $chunk_num $DATA_VERSION $QUERY_VERSION;
 done
