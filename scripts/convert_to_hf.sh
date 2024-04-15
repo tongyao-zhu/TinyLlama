35eb4d1f5aac8d8a1f1e4a2597978acba6ce7531
tongyao-zhu <312404930zhu@gmail.com>
2024-04-15 15:51:36 +0800

    change the model to be converted

--- scripts/convert_to_hf.sh
+++ scripts/convert_to_hf.sh
@@ -8,19 +8,38 @@
 #LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tiny_LLaMA_120M_8k_rpwiki_en_8k
 #CKPT_PREFIX=iter-020000-ckpt
 
-LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tiny_LLaMA_360M_8k_c4_news_8k
-CKPT_PREFIX=iter-030000-ckpt
+#LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tiny_LLaMA_360M_8k_c4_news_8k
+#CKPT_PREFIX=iter-030000-ckpt
+#
+#LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tiny_LLaMA_120M_8k_c4_news_8k
+#CKPT_PREFIX=iter-030000-ckpt
 
-LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tiny_LLaMA_120M_8k_c4_news_8k
-CKPT_PREFIX=iter-030000-ckpt
+#LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tiny_LLaMA_360M_8k_cc_merged_v1_8k
+#CKPT_PREFIX=iter-100000-ckpt-step-30000
+
+#LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tiny_LLaMA_360M_8k_cc_8k
+#CKPT_PREFIX=iter-105000-ckpt-step-30000
+
+LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tiny_LLaMA_360M_8k_cc_8k
+CKPT_PREFIX=iter-200000-ckpt-step-50000
+
+LIT_CKPT_DIR=/home/aiops/zhuty/tinyllama/out/tiny_LLaMA_120M_8k_cc_8k
+CKPT_PREFIX=iter-200000-ckpt-step-50000
+
+# if tiny_LLaMA_120M_8k in the model_name, set model name
+if [[ $LIT_CKPT_DIR == *"tiny_LLaMA_120M_8k"* ]]; then
+  MODEL_NAME=tiny_LLaMA_120M_8k
+else
+  MODEL_NAME=tiny_LLaMA_360M_8k
+fi
 
 python convert_lit_checkpoint.py \
 --checkpoint_name $CKPT_PREFIX.pth \
 --out_dir $LIT_CKPT_DIR \
---model_name tiny_LLaMA_120M_8k \
+--model_name $MODEL_NAME \
 --model_only false
 
-mkdir $LIT_CKPT_DIR/hf_ckpt
+mkdir -p $LIT_CKPT_DIR/hf_ckpt
 # move config
 mv $LIT_CKPT_DIR/config.json $LIT_CKPT_DIR/hf_ckpt/config.json
 mv $LIT_CKPT_DIR/$CKPT_PREFIX.bin $LIT_CKPT_DIR/hf_ckpt/pytorch_model.bin
