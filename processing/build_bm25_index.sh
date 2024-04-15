910b8853e10725b956d9eb6902d2e13e8f13e8b3
tongyao-zhu <312404930zhu@gmail.com>
2024-04-15 15:34:50 +0800

    typo fix

--- processing/build_bm25_index.sh
+++ processing/build_bm25_index.sh
@@ -6,7 +6,7 @@ export PATH=$PATH:$JAVA_HOME/bin
 VERSION=$1
 
 INPUT_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$VERSION/train
-INDEX_PATH=/home/aiops/zhuty/ret_pretraining_data//id_added/$VERSION/bm25_index
+INDEX_PATH=/home/aiops/zhuty/ret_pretraining_data/id_added/$VERSION/bm25_index
 python -m pyserini.index.lucene \
   --collection JsonCollection \
   --input $INPUT_PATH \
