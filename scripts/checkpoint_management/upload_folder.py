f3754cdc8a5f3e302dc09faf01a83e64367689fe
tongyao-zhu <312404930zhu@gmail.com>
2024-05-20 17:28:47 +0800

    Update upload_folder.py

--- scripts/checkpoint_management/upload_folder.py
+++ scripts/checkpoint_management/upload_folder.py
@@ -23,12 +23,31 @@ api.create_repo(
     exist_ok=True,
 )
 
-message = "Uploading folder {} to hf".format(local_path) + "tyzhu/{}".format(repo_path)[:64]
-message += "at time {}".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-api.upload_folder(
-    folder_path=local_path,
-    repo_id=repo_path,
-    repo_type=repo_type,
-    token = json.load(open("/home/aiops/zhuty/hf_token.json")),
-    commit_message=message,
-)
+
+for file in os.listdir(local_path):
+    # if it is a folder, upload the folder
+    if os.path.isdir(os.path.join(local_path, file)):
+        message = "Uploading folder {} to hf".format(local_path) + "tyzhu/{}".format(repo_path)[:64]
+        message += "at time {}".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        print("Uploading folder {} to hf".format(file) + "tyzhu/{}".format(repo_path)[:64])
+        api.upload_folder(
+            folder_path=os.path.join(local_path, file),
+            path_in_repo=file,
+            repo_id=repo_path,
+            repo_type=repo_type,
+            token = json.load(open("/home/aiops/zhuty/hf_token.json")),
+            commit_message=message,
+        )
+    else:
+        print("Uploading file {} to hf".format(file) + "tyzhu/{}".format(repo_path)[:64])
+        message = "Uploading file {} to hf".format(local_path) + "tyzhu/{}".format(repo_path)[:64]
+        message += "at time {}".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        # if it is a file, upload the file
+        api.upload_file(
+            path_or_fileobj=os.path.join(local_path, file),
+            path_in_repo=file,
+            repo_id=repo_path,
+            repo_type=repo_type,
+            token = json.load(open("/home/aiops/zhuty/hf_token.json")),
+            commit_message=message,
+        )
