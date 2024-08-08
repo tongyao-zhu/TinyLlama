3387741d494413069ac3661b90f6fd0f1c70ade7
tongyao-zhu <312404930zhu@gmail.com>
2024-08-08 18:24:48 +0800

    add scripts to trigger jobs

---
+++ scripts/compass_eval/run.sh
@@ -0,0 +1,8 @@
+
+
+
+python run.py --datasets wikitext_103_raw_ppl  --hf-path tyzhu/tiny_LLaMA_1b_8k_intramask_cc_8k_iter-480000-ckpt-step-60000_hf --num-gpus 1
+
+# tasks selected
+wikitext_103_raw_ppl
+wikitext_2_raw_ppl
